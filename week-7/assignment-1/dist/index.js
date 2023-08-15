476104d51d5925f0a4075b649fb7ed797ad47aba
Shashwat Singh <shashwat123student@gmail.com>
2023-08-15 12:38:12 +0530

    Fixed the import

--- week-7/assignment-1/dist/index.js
+++ week-7/assignment-1/dist/index.js
@@ -1,19 +1,15 @@
-
-import express from "express";
+const express = require("express");
 const app = express();
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 const port = 3000;
-import authRoutes from "./routes/auth";
-import todoRoute from "./routes/todo";
+const authRoutes = require("./routes/auth");
+const todoRoutes = require("./routes/todo");
 const cors = require("cors");
-
 app.use(cors());
 app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/todo", todoRoutes);
-
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
-
+    console.log(`Example app listening at http://localhost:${port}`);
+});
 mongoose.connect('mongodb+srv://ShashwatPS:s@cluster0.1alkv6j.mongodb.net/courses', { dbName: "courses" });
